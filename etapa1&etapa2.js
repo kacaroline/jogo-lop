c0c395189a15561808ccee1d41b216c5f3a1af54
Raffa267 <48894081+Raffae2679@users.noreply.github.com>
2019-04-25 22:31:19 -0300

    Create etapa1&etapa2.js
    Adicionado as figuras (quadrado e bola), onde representa obstaculo e jogador. Adicionado tambem movimentação através das setas do teclado

---
+++ etapa1&etapa2.js
@@ -0,0 +1,52 @@
+/* 
+    Equipe: 
+        Kauana Caroline - Subturma D (Líder) 
+        Raffael Morais Dutra - Subturma C 
+        Etapa 1 & 2
+*/
+
+x=200;
+y=300;
+xq=175;
+function setup() {
+  createCanvas(400, 400);
+   
+}
+function draw() {
+  background("grey");
+  //Comando de Movimento
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 10;
+    if(x<-15){
+      x=400
+    }
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += 10;
+    if(x>415){
+      x=0
+    }
+    
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    y -= 10;
+    if(y<-15){
+      y=400
+    }
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    y += 10;
+    if(y>415){
+      y=0
+    }
+  }
+  
+  ellipse(x, y,40, 50);
+  fill("red")
+  rect(175,20,40,50);
+  fill("blue")
+  
+}
